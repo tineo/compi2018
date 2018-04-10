b762ad718aa6ba47c0739cac2eac3511f8de3ad4
tineo <cesar@tineo.mobi>
2018-04-10 02:32:00 -0500

    add .idea config

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import comp.Ventana;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello World!");
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+}
