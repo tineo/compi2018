9d1e5b9b00cda6107caad943c4cbadc604aa35a1
Renzo <wmunozarellano@gmail.com>
2018-04-10 19:41:57 -0500

    |agregue validacion y separacion de id y tipo

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,44 @@ import org.apache.commons.io.*;
 
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
         //System.out.println("Hello World!");
+
+
+        String input = "hello I'm a java dev" +
+                "no job experience needed" +
+                "senior software engineer" +
+                "java job available for senior software engineer";
+
+        String fixedInput = input.replaceAll("(java|job|senior)", "<b>$1</b>");
+        System.out.println(fixedInput);
+
+
+        String types = "int|float|double|long";
+        String nodeValue = "int a =  13 + b ; ";
+
+        ArrayList<String> regexs = new ArrayList<>();
+
+
+        //regexs.add("\\s*(int|float|double|long) \\w{1}\\s*=\\s*\\d*\\s*;\\s*"); //<type> <id>=<value>;
+        regexs.add("\\s*(int|float|double|long) \\w{1}(\\s*|\\s*=\\s*\\d*\\s*);\\s*"); //<type> <id>( |=<value>);
+        regexs.add("\\s*\\w{1}\\s*=\\s*(\\d*|\\{w{1}})\\s*;\\s*"); //<id> = <value>|<id>;
+        regexs.add("\\s*(\\w{1}|(int|float|double|long) \\w{1})\\s*=\\s*(\\w{1}|\\d+)\\s*(\\+|\\-|\\*|\\/)\\s*(\\w{1}|\\d+)\\s*;\\s*");
+
         Ventana v = new Ventana();
 
         v.btnElegir.addActionListener(e -> {
 
             JFileChooser fileChooser = new JFileChooser();
-            FileNameExtensionFilter filter = new FileNameExtensionFilter("TEXT FILES", "txt", "text");
+            FileNameExtensionFilter filter = new FileNameExtensionFilter("*.txt", "txt", "text");
             fileChooser.setFileFilter(filter);
             int seleccion = fileChooser.showOpenDialog(v);
 
@@ -27,11 +52,60 @@ public class Main {
 
                 v.txtOrigen.setText(fichero.getAbsolutePath());
 
+                HashMap<String, String> mapa = new HashMap<>();
 
                 try {
-                    String nodeValue = "i am mostafa";
 
-                    String fname = FilenameUtils.getBaseName(fichero.getAbsolutePath())+"_nuevo."+FilenameUtils.getExtension(fichero.getAbsolutePath());
+                    try(BufferedReader br = new BufferedReader(new FileReader(fichero))) {
+                        boolean cumple = false;
+                        for(String line; (line = br.readLine()) != null; ) {
+                            //System.out.println(line);
+
+
+                            for (String regex : regexs) {
+                                //System.out.println(regex);
+                                cumple = Pattern.matches(regex, line);
+                                if(cumple) break;
+                            }
+
+                            if(!cumple){
+                                System.out.println(line);
+                            }else{
+
+                                if(Pattern.matches(regexs.get(0), line)){
+                                    System.out.println("line: "+line);
+
+                                    Pattern p = Pattern.compile("(int|float|double|long) (\\w{1})(\\s*|\\s*=\\s*\\d*\\s*);");
+                                    Matcher m = p.matcher(line);
+                                    while(m.find()) {
+                                        System.out.println(m.group(1));
+                                        System.out.println(m.group(2));
+                                        mapa.put(m.group(1), m.group(2));
+
+                                    }
+
+
+                                }
+
+
+                            }
+
+
+                        }
+
+
+
+                    }
+
+                     //(<id>|<type> <id>)=(<value>|<id>)<op>(<value>|<id>);
+
+
+
+
+
+
+
+                    /*String fname = FilenameUtils.getBaseName(fichero.getAbsolutePath())+"_nuevo."+FilenameUtils.getExtension(fichero.getAbsolutePath());
                     String faname = FilenameUtils.concat(fichero.getParent(),fname);
 
                     File nuevofichero = new File(faname);
@@ -43,7 +117,7 @@ public class Main {
                     }
                     writer.close();
 
-                    v.txtSalida.setText(faname);
+                    v.txtSalida.setText(faname);*/
                 }catch (Exception ex){
                     ex.printStackTrace();
                 }
